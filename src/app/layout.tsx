feb4684e2a617ce9ad0f5f7cfc5dc5bb08ecb31c
superchoco3000 <lucascasanove@yahoo.fr>
2026-04-12 19:09:50 +0200

    Add real app icon to PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="fr"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       
